fe79645de7d41d60a29b57ef7076d312bb5ee5fd
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-09-19 16:52:41 -0700

    thingies

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,8 +20,10 @@ export const putDb = async (content) => {
   const objStore = transact.objectStore('jate'); // returns an IDBObjectStore in the scope of the transaction
   const request = objStore.put({ id: 1, value: content }); // pass the content with an id of 1 into the store using the put method
   const result = await request;
+  console.log(result);
   //console.log('data saved to database: ', result.value) // confirm the request
-  console.log('data saved to database: ', result);
+  //result ? console.log('🚀 data saved to database: ', result.value) : console.log('🚀 Data not found in the database.')
+  console.log('Your latest changes have been saved to the database.');
 }
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -33,7 +35,7 @@ export const getDb = async () => {
   const request = objStore.get(1); // get all values that have an id of 1 from the database
   const result = await request;
   console.log('result.value: ', result.value);
-  return result.value;
+  return result?.value;
 };
 
 initdb();
