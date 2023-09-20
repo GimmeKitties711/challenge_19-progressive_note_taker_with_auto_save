1115a1b3448bf6bd706f8c3a8b59e1d21753d0ac
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-09-20 10:07:55 -0700

    resolved errors, got program to work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,10 +20,18 @@ export const putDb = async (content) => {
   const objStore = transact.objectStore('jate'); // returns an IDBObjectStore in the scope of the transaction
   const request = objStore.put({ id: 1, value: content }); // pass the content with an id of 1 into the store using the put method
   const result = await request;
-  console.log(result);
-  //console.log('data saved to database: ', result.value) // confirm the request
-  //result ? console.log('🚀 data saved to database: ', result.value) : console.log('🚀 Data not found in the database.')
-  console.log('Your latest changes have been saved to the database.');
+  result ? console.log('Data saved to database: ' + result + ' (successful)') : console.log('Data not put into the database successfully.')
+  /*
+  this ternary expression means:
+
+  if (result) {
+    console.log('Data saved to database: ' + result + ' (successful)');
+  } else {
+    console.log('Data not found in the database.');
+  }
+
+  the if condition is used if result is 1 (truthy), otherwise the else condition is used (result is falsy).
+  */
 }
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -34,8 +42,8 @@ export const getDb = async () => {
   const objStore = transact.objectStore('jate');
   const request = objStore.get(1); // get all values that have an id of 1 from the database
   const result = await request;
-  console.log('result.value: ', result.value);
-  return result?.value;
+  result ? console.log('result.value: ', result.value) : console.log('Data not found in the database. Get started by typing text in the window. Save the text by clicking off of the window.')
+  return result?.value; // the question mark at the end of result is called optional chaining. if result is undefined/null, result?.value returns undefined instead of throwing an error. source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
 };
 
 initdb();
